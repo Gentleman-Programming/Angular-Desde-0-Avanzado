c77766b6d883d1f5d65e7c13c7f2e3e5f439e00a
alan buscaglia <alanbuscaglia@gmail.com>
2023-08-31 22:57:47 +0200

    feature: clase-4-directivas

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+  standalone: true,
+})
+export class HighlightDirective {
+  constructor(private el: ElementRef) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.el.nativeElement.style.backgroundColor = 'yellow';
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = '';
+  }
+}
